73a268c4151c9f204c6d50213b5006b78c7cce48
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2014-03-19 21:17:58 -0400

    check if on pi correctly-ier

--- onpi.py
+++ onpi.py
@@ -2,4 +2,4 @@ import subprocess
 
 def onPi():
   uname_m = subprocess.Popen(["uname", "-m"], stdout = subprocess.PIPE, stderr = subprocess.STDOUT).communicate()[0]
-  return uname_m == "armv6l"
+  return uname_m.strip() == "armv6l"
