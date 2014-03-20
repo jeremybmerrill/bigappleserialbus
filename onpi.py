b453fc9e7f1399ee1934d7dfa0cae594d051e8b9
Jeremy B. Merrill <jeremybmerrill@gmail.com>
2014-03-19 21:16:22 -0400

    check if on pi correctly

--- onpi.py
+++ onpi.py
@@ -1,5 +1,5 @@
-from subprocess import call
+import subprocess 
 
 def onPi():
-  uname_m = call(["uname", "-m"])
+  uname_m = subprocess.Popen(["uname", "-m"], stdout = subprocess.PIPE, stderr = subprocess.STDOUT).communicate()[0]
   return uname_m == "armv6l"
